e497ee751fb2849d7413c46be82981f07c00ed18
korolka <korolka212@gmail.com>
2023-08-27 10:31:33 +0300

    fCom

---
+++ homeworkLesson1Asp.NetCoreEss/Controllers/HomeController.cs
@@ -0,0 +1,35 @@
+﻿using homeworkLesson1Asp.NetCoreEss.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace homeworkLesson1Asp.NetCoreEss.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            OperatingSystem system = Environment.OSVersion;
+            string message = system.VersionString;// Platform.ToString();
+            ViewBag.Message = message;
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ homeworkLesson1Asp.NetCoreEss/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace homeworkLesson1Asp.NetCoreEss.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
